c67ed97eba3d09a3705f6e494b8e910f43396f7c
Raul Ramirez <raul.ramtap@gmail.com>
2024-05-22 00:41:22 -0400

    initial release

--- lib/tqsft-vpc-stack.ts
+++ lib/tqsft-vpc-stack.ts
@@ -1,15 +1,61 @@
-import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import { Stack, Construct, StackProps, RemovalPolicy } from '@aws-cdk/core';
+import { Vpc, SubnetType, SecurityGroup} from '@aws-cdk/aws-ec2';
+import { StringParameter } from '@aws-cdk/aws-ssm'
+import { FileSystem } from '@aws-cdk/aws-efs'
 
-export class TqsftVpcStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class TqsftVpcStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const myVpc = new Vpc(this, 'VPC', {
+      cidr: process.env.VPC_CIDR,
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          name: 'PublicSubnet',
+          cidrMask: 26,
+          subnetType: SubnetType.PUBLIC
+        },{
+          name: 'PrivateSubnet',
+          cidrMask: 26,
+          subnetType: SubnetType.PRIVATE_ISOLATED
+        }
+      ],
+      vpcName: 'Tqsft-Vpc',
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TqsftVpcQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const efsSG = new SecurityGroup(this, "EFS-SG", {
+      vpc: myVpc,
+      securityGroupName: "EFSSecurityGroup",
+      description: "Shared Drive for ECS",
+      allowAllOutbound: true
+    });
+
+    const efs = new FileSystem(this, "CodeServerFS", {
+      vpc: myVpc,
+      fileSystemName: "ECSClusterSharedEFS",
+      securityGroup: efsSG,
+      vpcSubnets: {
+        subnetType: SubnetType.PRIVATE_ISOLATED
+      },
+      removalPolicy: RemovalPolicy.DESTROY
+    })
+
+    new StringParameter(this, 'TqsftStackVpcId', {
+      parameterName: 'TqsftStack-VpcId',
+      stringValue: myVpc.vpcId
+    });
+
+    new StringParameter(this, 'TqsftStackFsSgId', {
+      parameterName: 'TqsftStack-FsSgId',
+      stringValue: efsSG.securityGroupId
+    })
+
+    new StringParameter(this, 'TqsftStackFsId', {
+      parameterName: 'TqsftStack-FsId',
+      stringValue: efs.fileSystemId
+    })
   }
 }
